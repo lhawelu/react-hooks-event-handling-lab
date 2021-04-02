ae57546363925629d549995b8bd1f135f8fd6d8b
Larry Hawelu <lhawelu@gmail.com>
2021-04-02 10:01:50 -0700

    commit tree 55f47c3e2cf32a6c3b880a96cef34aa8d1b2b4bd

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+class EyesOnMe extends Component {
+
+  render() {
+
+    return(
+      <div>
+        <button onFocus={(e) => console.log('Good!')} onBlur={(e) => console.log('Hey! Eyes on me!')}>
+          Eyes on me
+        </button>
+      </div>
+    )
+  }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
-// Code Keypad Component Here
+import React, { Component } from 'react'
+
+class Keypad extends Component {
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label>
+            Password:
+            <input onChange={(e) => console.log('Entering password...')}type='password' />
+          </label>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Keypad
